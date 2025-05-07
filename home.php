3a42dcfa1ac17e0189e886984d070098b649a2cc
khinkyaw666 <khinkhinkyawthat4@gmail.com>
2025-05-07 20:35:59 +0630

    first init

---
+++ home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .body{
+            background: darkblue;
+            color:white;
+            text-align:center;
+
+        }
+    </style>
+</head>
+<body class="body">
+    <?php
+    echo "<h1>She is living in mandalay</h1>";
+    echo "<table>
+    <tbody>
+      <th>no</th>
+      <td>Shwe Shwe</td>
+      <th>name</th>
+      <th>age</th>
+         
+    </tbody>
+    
+    </table>";
+
+    ?>
+    
+</body>
+</html>
